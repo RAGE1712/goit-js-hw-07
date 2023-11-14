4e59e7990000d64a0f2396d1285e5c91d4a7385e
RAGE1712 <137414196+RAGE1712@users.noreply.github.com>
2023-11-14 17:53:25 +0200

    DZ1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector(".gallery");
 
-console.log(galleryItems);
+list.insertAdjacentHTML("beforeend", galleryMarkup(galleryItems));
+
+function galleryMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>
+    </li>
+    `
+    )
+    .join("");
+}
+
+list.addEventListener("click", handleClick)
+
+function handleClick(event) {
+    event.preventDefault()
+  if (event.target === event.currentTarget) {
+    return;
+     }
+    const currentImageSrc = event.target.dataset.source;
+    const bigImage = `<img src="${currentImageSrc}" width="800" height="600">`;
+    const instance = basicLightbox.create(bigImage);
+
+    instance.show();
+    document.addEventListener("keydown", closeModal);
+
+    function closeModal(event) {
+      if (event.key === "Escape") {
+        instance.close();
+        document.removeEventListener("keydown", closeModal);
+      }
+    }    
+}
+
