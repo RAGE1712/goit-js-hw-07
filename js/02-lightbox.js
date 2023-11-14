aebabf8f109ecc3bb3ce395817975d553b105299
RAGE1712 <137414196+RAGE1712@users.noreply.github.com>
2023-11-14 19:32:17 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector(".gallery");
 
-console.log(galleryItems);
+list.insertAdjacentHTML("beforeend", galleryMarkup(galleryItems));
+
+function galleryMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        />
+    </a>
+    </li>
+    `
+    )
+    .join("");
+}
+
+list.addEventListener("click", handleClick);
+
+function handleClick(event) {
+    event.preventDefault();
+    var lightbox = new SimpleLightbox(".gallery a", {
+      captionsData: "alt",
+      captionPosition: "bottom",
+      captionDelay: 250,
+      //   overlay: false
+    //   fadeSpeed: 1
+    });
+
+}
